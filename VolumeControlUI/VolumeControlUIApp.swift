706cfd7ad6d34cea21de7f96cac0f3f35ef8b772
Andrey <69643088+AndySLO@users.noreply.github.com>
2021-10-12 21:50:20 +0200

    Initial Commit

---
+++ VolumeControlUI/VolumeControlUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolumeControlUIApp.swift
+//  VolumeControlUI
+//
+//  Created by AndreyP on 12/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct VolumeControlUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
